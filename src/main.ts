d87a15c77329bda4f62ef06c256120265ba9bdd0
s.pinchukov <s.pinchukov@texrus.com>
2025-05-04 23:39:15 +0300

    Clean trash

--- src/main.ts
+++ src/main.ts
@@ -19,20 +19,3 @@ async function bootstrap() {
   await app.listen(env.PORT ?? 4200); // 8080
 }
 bootstrap();
-
-// https://www.youtube.com/watch?v=-7K_0NRLCu4&list=PLG3k6fiOzfyMk72M0DU4gK5gJS_ZFZ2RQ
-
-// 30.00 - nestjs создание и тд
-// 37/30 - настройка prisma
-// 54.50 - настройка prisma в самом nestjs
-
-
-// 1.03 - авторизация
-// 1.22 - логин и регистрация 
-// 1.46 закончил
-
-// $ nest generate module test
-// $ nest generate controller test
-// $ nest generate service test
-
-// nest g resource post --no-spec !!!!!!!
